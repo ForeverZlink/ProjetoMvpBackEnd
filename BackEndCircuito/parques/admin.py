acf98e8b3a7639a6bb5739571887f6e63282d003
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2025-11-09 10:40:27 -0300

    Adicionado in line no django admin para o usuário ser capaz de adicionar tags aos parques

--- BackEndCircuito/parques/admin.py
+++ BackEndCircuito/parques/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Endereco, HorarioFuncionamento, Parque
+from .models import Endereco, HorarioFuncionamento, Parque,Tag
 
 
 # Exibição de horários diretamente dentro do parque
 class HorarioFuncionamentoInline(admin.TabularInline):
     model = HorarioFuncionamento
+    extra = 1
+class TagInline(admin.TabularInline):
+    model = Tag
     extra = 1
 
 
@@ -12,7 +15,7 @@ class HorarioFuncionamentoInline(admin.TabularInline):
 class ParqueAdmin(admin.ModelAdmin):
     list_display = ("nome_do_parque", "descricao", "site", "get_endereco_resumido")
     search_fields = ("nome_do_parque", "descricao")
-    inlines = [HorarioFuncionamentoInline]
+    inlines = [HorarioFuncionamentoInline,TagInline]
 
     def get_endereco_resumido(self, obj):
         """Exibe o endereço de forma resumida na lista."""
@@ -27,6 +30,12 @@ class EnderecoAdmin(admin.ModelAdmin):
     list_display = ("cep", "logradouro", "bairro", "cidade", "estado")
     search_fields = ("cep", "logradouro", "bairro", "cidade")
     list_filter = ("estado",)
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
+    list_display = ("nome_da_tag","parque")
+    search_fields = ("nome_da_tag","parque")
+    list_filter = ("nome_da_tag","parque")
 
 
 @admin.register(HorarioFuncionamento)
--- BackEndCircuito/parques/models.py
+++ BackEndCircuito/parques/models.py
@@ -77,6 +77,18 @@ class HorarioFuncionamento(models.Model):
 # ------------------------------
 # 🏞️ Parque
 # ------------------------------
+class Tag(models.Model):
+    nome_da_tag = models.CharField(max_length=300, verbose_name="Tag")
+    parque = models.ForeignKey(
+        'Parque',
+        on_delete=models.CASCADE,
+        related_name='tag',
+        null=True,
+        blank=True
+    )
+    def __str__(self):
+        return self.nome_da_tag
+    
 class Parque(models.Model):
     nome_do_parque = models.CharField(max_length=300, verbose_name="Nome do Parque")
     descricao = models.TextField(blank=True, null=True)
